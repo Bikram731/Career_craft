a1008942a9b5b2d1d81d889b271da1fb8dc221a3
Bikram731 <bikramdeep0999@gmail.com>
2026-03-13 22:49:49 +0530

    Delete backend - Copy directory

--- backend - Copy/middleware/isAdmin.js
+++
@@ -1,6 +0,0 @@
-module.exports = function isAdmin(req, res, next) {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ message: "Access denied. Admins only." });
-  }
-  next();
-};
